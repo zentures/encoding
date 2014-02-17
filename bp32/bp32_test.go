f3cda05de5086748072595ea7c6f7bc08bf0aced
Daniel Lemire <lemire@gmail.com>
2014-02-17 16:38:32 -0500

    Making unit tests less excessively long. Improving the perf. of fastpfor by about 10%.

--- bp32/bp32_test.go
+++ bp32/bp32_test.go
@@ -9,13 +9,14 @@ package bp32
 import (
 	"github.com/reducedb/encoding/benchtools"
 	"github.com/reducedb/encoding/generators"
+        "github.com/reducedb/encoding/cursor"
 	"log"
 	"testing"
 )
 
 var (
 	data []int32
-	size int = 12800000
+	size int = 128000
 )
 
 func init() {
@@ -25,6 +26,25 @@ func init() {
 }
 
 func TestCodec(t *testing.T) {
-	sizes := []int{128, 128 * 10, 128 * 100, 128 * 1000, 128 * 10000}
+	sizes := []int{128, 128 * 10, 128 * 100, 128 * 1000}
 	benchtools.TestCodec(New(), data, sizes)
+}
+
+// go test -bench=Decode
+func BenchmarkDecode(b *testing.B) {
+       b.StopTimer()
+       length := 128 * 1024
+       data := generators.GenerateClustered(length, 1<<24)
+       compdata := make([]int32, 2*length)
+       recov := make([]int32, length)
+       inpos := cursor.New()
+       outpos := cursor.New()
+       codec := New()
+       codec.Compress(data, inpos, len(data), compdata, outpos)
+        b.StartTimer()
+       for j := 0; j < b.N; j++ {
+               newinpos := cursor.New()
+               newoutpos := cursor.New()
+               codec.Uncompress(compdata, newinpos, outpos.Get()-newinpos.Get(), recov, newoutpos)
+       }
 }
